ec53319d019bc14baef411a8141fda5a8992f021
Gabriel Elbaz <gelbaz@g.jct.ac.il>
2019-11-12 18:44:32 +0200

    Fin du parse "Communication protocol-1" I et II.1-7,9,11,12 dans "Communication Protocol"

--- src/__tests__/parser.test.js
+++ src/__tests__/parser.test.js
@@ -1,12 +1,41 @@
 import parse from "../index";
 
-const packet1 =
-  "[3G*5305994463*0178*UD,011119,103406,V,31.766222,N,35.190035,E,0.00,0.0,0.0,4,95,24,0,205,00000000,7,255,425,1,10472,10013,142,10471,10611,135,10472,10053,134,10471,10612,130,10471,10102,130,10471,10563,129,10472,11413,129,5,JCT-Lev-WiFi,b4:5d:50:bd:8c:e0,-80,JCT-Lev-WiFi,b4:5d:50:bd:99:80,-83,Home2,18:d6:c7:ef:1e:a7,-86,TP-LINK_AKA,3c:46:d8:c6:9a:f4,-90,JCT-Lev-WiFi,b4:5d:50:bd:c1:c0,-91,27.7]";
+const packetLK = "[SG*8800000015*0002*LK]";
+const packetLKE = "[SG*8800000015*000D*LK,50,100,100]";
+const packetUD =
+  "[SG*5305994463*0178*UD,011119,103406,V,31.766222,N,35.190035,E,0.00,0.0,0.0,4,95,24,0,205,00000000,7,255,425,1,10472,10013,142,10471,10611,135,10472,10053,134,10471,10612,130,10471,10102,130,10471,10563,129,10472,11413,129,5,JCT-Lev-WiFi,b4:5d:50:bd:8c:e0,-80,JCT-Lev-WiFi,b4:5d:50:bd:99:80,-83,Home2,18:d6:c7:ef:1e:a7,-86,TP-LINK_AKA,3c:46:d8:c6:9a:f4,-90,JCT-Lev-WiFi,b4:5d:50:bd:c1:c0,-91,27.7]";
+const packetUD2 =
+  "[SG*8800000015*0088*UD2,220414,134652,A,22.571707,N,113.8613968,E,0.1,0.0,100,7,60,90,1000,50,0000,4,1,460,0,9360,4082,131,9360,4092,148,9360,4091,143,9360,4153,141]";
+const packetAL =
+  "[SG*8800000015*0087*AL,220414,134652,A,22.571707,N,113.8613968,E,0.1,0.0,100,7,60,90,1000,50,0001,4,1,460,0,9360,4082,131,9360,4092,148,9360,4091,143,9360,4153,141]";
+const packetUPLOAD = "[SG*8800000015*0009*UPLOAD,10]";
+const packetMONITOR = "[SG*8800000015*0007*MONITOR]";
+const packetWAD =
+  "[SG*8800000015*008B*WAD,CH,220414,134652,A,22.571707,N,113.8613968,E,0.1,0.0,100,7,60,90,1000,50,0001,4,1,460,0,9360,4082,131,9360,4092,148,9360,4091,143,9360,4153,141]";
+const packetWG =
+  "[SG*8800000015*0087*WG,220414,134652,A,22.571707,N,113.8613968,E,0.1,0.0,100,7,60,90,1000,50,0001,4,1,460,0,9360,4082,131,9360,4092,148,9360,4091,143,9360,4153,141]";
 
-describe("parse", () => {
+describe("parse packets", () => {
   it("should parse update packets correctly", () => {
-    const expected = {
-      vendor: "3G",
+    const expectedLK = {
+      vendor: "SG",
+      id: "8800000015",
+      length: 2,
+      actionType: "LK"
+    };
+
+    const expectedLKE = {
+      vendor: "SG",
+      id: "8800000015",
+      length: parseInt("0x000d", 16),
+      actionType: "LK",
+      steps: 50,
+      rollingTime: 100,
+      batteryAmount: 100
+    };
+
+    const expectedUD = {
+      vendor: "SG",
       id: "5305994463",
       length: 178,
       actionType: "UD",
@@ -15,7 +44,170 @@ describe("parse", () => {
       longitude: 35.190035
     };
 
-    expect(parse(packet1)).toEqual(expected);
-    expect(1).toBe(1);
+    const expectedUD2 = {
+      vendor: "SG",
+      id: "8800000015",
+      length: 88,
+      actionType: "UD2",
+      date: new Date("04/22/2014 13:46:52"), // mm/dd/yyyy hh:mm:ss
+      latitude: 22.571707,
+      longitude: 113.8613968
+    };
+
+    const expectedAL = {
+      vendor: "SG",
+      id: "8800000015",
+      length: 87,
+      actionType: "AL",
+      date: new Date("04/22/2014 13:46:52"), // mm/dd/yyyy hh:mm:ss
+      latitude: 22.571707,
+      longitude: 113.8613968
+    };
+
+    const expectedUPLOAD = {
+      vendor: "SG",
+      id: "8800000015",
+      length: 9,
+      actionType: "UPLOAD",
+      interval: 10
+    };
+
+    const expectedMONITOR = {
+      vendor: "SG",
+      id: "8800000015",
+      length: 7,
+      actionType: "MONITOR"
+    };
+
+    const expectedWAD = {
+      vendor: "SG",
+      id: "8800000015",
+      length: 139,
+      actionType: "WAD",
+      language: "CH",
+      date: new Date("04/22/2014 13:46:52"), // mm/dd/yyyy hh:mm:ss
+      latitude: 22.571707,
+      longitude: 113.8613968
+    };
+
+    ("[SG*8800000015*0087*WG,220414,134652,A,22.571707,N,113.8613968,E,0.1,0.0,100,7,60,90,1000,50,0001,4,1,460,0,9360,4082,131,9360,4092,148,9360,4091,143,9360,4153,141]");
+    const expectedWG = {
+      vendor: "SG",
+      id: "8800000015",
+      length: 87,
+      actionType: "WG",
+      date: new Date("04/22/2014 13:46:52"), // mm/dd/yyyy hh:mm:ss
+      latitude: 22.571707,
+      longitude: 113.8613968
+    };
+
+    expect(parse(packetLK)).toEqual(expectedLK);
+    //expect(parse(packetLKE)).toEqual(expectedLKE);
+    expect(parse(packetUD)).toEqual(expectedUD);
+    expect(parse(packetUD2)).toEqual(expectedUD2);
+    expect(parse(packetAL)).toEqual(expectedAL);
+    expect(parse(packetUPLOAD)).toEqual(expectedUPLOAD);
+    expect(parse(packetMONITOR)).toEqual(expectedMONITOR);
+    //expect(parse(packetWAD)).toEqual(expectedWAD);
+    //expect(parse(packetWG)).toEqual(expectedWG);
+  });
+});
+
+// **** 2. SEND COMMANDS ON PLATFORM ***** //
+const packetCENTER = "[SG*8800000015*0012*CENTER,00000000000]";
+const packetSLAVE = "[SG*8800000015*0011*SLAVE,00000000000]";
+const packetPW = "[SG*8800000015*0009*PW,111111]";
+const packetCALL = "[SG*8800000015*0010*CALL,00000000000]";
+const packetSMS = "[SG*8800000015*001C*SMS,00000000000,123ABC How are you]";
+
+const packetUPGRADE =
+  "[SG*8800000015*0039*UPGRADE,http://www.3g-elec.com/g29_updata/test/jt_ads.bin]";
+const packetIP = "[SG*8800000015*0014*IP,113.81.229.9,5900]";
+const packetFACTORY = "[SG*8800000015*0007*FACTORY]";
+const packetLZ = "[SG*8800000015*0006*LZ,1,8]";
+
+describe("parse packets: 2. SEND COMMANDS ON PLATFORM", () => {
+  it("should parse correction part 2 packets", () => {
+    const expectedCENTER = {
+      vendor: "SG",
+      id: "8800000015",
+      length: 12,
+      actionType: "CENTER",
+      centerNumber: "00000000000"
+    };
+
+    const expectedSLAVE = {
+      vendor: "SG",
+      id: "8800000015",
+      length: 11,
+      actionType: "SLAVE",
+      assistanceNumber: "00000000000"
+    };
+
+    const expectedPW = {
+      vendor: "SG",
+      id: "8800000015",
+      length: 9,
+      actionType: "PW",
+      password: "111111"
+    };
+
+    const expectedCALL = {
+      vendor: "SG",
+      id: "8800000015",
+      length: 10,
+      actionType: "CALL",
+      phoneNumber: "00000000000"
+    };
+
+    const expectedSMS = {
+      vendor: "SG",
+      id: "8800000015",
+      length: 28, // 1C
+      actionType: "SMS",
+      phoneNumber: "00000000000",
+      message: "123ABC How are you"
+    };
+
+    const expectedUPGRADE = {
+      vendor: "SG",
+      id: "8800000015",
+      length: 39, // 1C
+      actionType: "UPGRADE",
+      URL: "http://www.3g-elec.com/g29_updata/test/jt_ads.bin"
+    };
+
+    const expectedIP = {
+      vendor: "SG",
+      id: "8800000015",
+      length: 14, // 1C
+      actionType: "IP",
+      IP: "113.81.229.9",
+      port: 5900
+    };
+
+    const expectedFACTORY = {
+      vendor: "SG",
+      id: "8800000015",
+      length: 7, // 0007
+      actionType: "FACTORY"
+    };
+
+    const expectedLZ = {
+      vendor: "SG",
+      id: "8800000015",
+      language: "1",
+      timeArea: "8"
+    };
+
+    expect(parse(packetCENTER)).toEqual(expectedCENTER);
+    expect(parse(packetSLAVE)).toEqual(expectedSLAVE);
+    expect(parse(packetPW)).toEqual(expectedPW);
+    expect(parse(packetCALL)).toEqual(expectedCALL);
+    //expect(parse(packetSMS)).toEqual(expectedSMS);
+    expect(parse(packetUPGRADE)).toEqual(expectedUPGRADE);
+    expect(parse(packetIP)).toEqual(expectedIP);
+    expect(parse(packetFACTORY)).toEqual(expectedFACTORY);
+    expect(parse(packetLZ)).toEqual(expectedLZ);
   });
 });
--- src/parser.js
+++ src/parser.js
@@ -1,4 +1,5 @@
 export const parse = str => {
+  // Parse header
   const arr = str.substr(1, str.length - 2).split(","); // Get rid of leading and trailing angle brackets
   const header = parseHeader(arr[0]);
 
@@ -6,15 +7,93 @@ export const parse = str => {
   let body = arr.slice(1, arr.length);
 
   switch (header.actionType) {
-    case "UD":
-      body = parseUpdateBody(body);
+    // **** 1. TERMINAL COMMANDS ***** //
+    case "LK":
+      body = parseLK(body);
       break;
+    case "UD":
+      body = parseUD(body);
+      break;
+    case "UD2":
+      body = parseUD2(body);
+      break;
+    case "AL":
+      body = parseAL(body);
+      break;
+    case "UPLOAD":
+      body = parseUPLOAD(body);
+      break;
+    case "MONITOR":
+      body = {}; // Any body to parse
+      break;
+    case "WAD":
+      body = parseWAD(body);
+      break;
+    case "WG":
+      body = parseWG(body);
+      break;
+    // **** 2. SEND COMMANDS ON PLATFORM ***** //
+    case "CENTER":
+      body = parseCENTER(body);
+      break;
+    case "SLAVE":
+      body = parseSLAVE(body);
+      break;
+    case "PW":
+      body = parsePW(body);
+      break;
+    case "CALL":
+      body = parseCALL(body);
+      break;
+    case "SMS":
+      body = parseSMS(body);
+      break;
+    case "*****************":
+      body = parseWG(body);
+      break;
+    case "UPGRADE":
+      body = parseUPGRADE(body);
+      break;
+    case "IP":
+      body = parseIP(body);
+      break;
+    case "FACTORY":
+      body = {}; // Any body to parse
+      break;
+    case "LZ":
+      body = parseLZ(body);
+      break;
+    case "*****************":
+      body = parseWG(body);
+      break;
+    case "*****************":
+      body = parseWG(body);
+      break;
+    case "*****************":
+      body = parseWG(body);
+      break;
+    case "*****************":
+      body = parseWG(body);
+      break;
+    case "*****************":
+      body = parseWG(body);
+      break;
+    case "*****************":
+      body = parseWG(body);
+      break;
+    case "*****************":
+      body = parseWG(body);
+      break;
+    case "*****************":
+      body = parseWG(body);
+      break;
+
     default:
       body = { error: "Action type unsupported" };
       break;
   }
 
-  // Fonction de retour
+  // Return function
   return {
     ...header,
     ...body
@@ -45,17 +124,119 @@ const _parseDateTime = (date, time) =>
     parseInt(time.substr(4, 2))
   );
 
+export const parseLK = body => {
+  console.log(body.length);
+  if (body.length === 0) return;
+  else {
+    const steps = parseInt(body[0]);
+    const rollingTime = parseInt(body[1]);
+    const batteryAmount = parseInt(body[2]);
+
+    return {
+      steps,
+      rollingTime,
+      batteryAmount
+    };
+  }
+};
+
 // Body format: date, time, local, latitude, latitudeSymbol, longitude, longitudeSymbol, ...
-export const parseUpdateBody = body => {
+export const parseLocationData = body => {
   const arr = body.slice(0, 7); // We only need the location data
 
   const date = _parseDateTime(arr[0], arr[1]);
-  const latitude = parseFloat(arr[3]);
-  const longitude = parseFloat(arr[5]);
+  // const local = arr[2]; // Local or Not Local Time
+
+  const latitude = parseFloat(arr[3]) * (arr[4] == "N" ? 1 : -1);
+  const longitude = parseFloat(arr[5]) * (arr[6] == "E" ? 1 : -1);
 
   return {
     date,
     latitude,
     longitude
+  };
+};
+
+export const parseUD = body => parseLocationData(body);
+
+export const parseUD2 = body => parseLocationData(body);
+
+export const parseAL = body => parseLocationData(body);
+
+export const parseUPLOAD = body => {
+  const interval = parseInt(body[0]);
+  return {
+    interval
+  };
+};
+
+export const parseWAD = body => {
+  const language = body[0];
+  const locationData = parseLocationData(body.slice(1, body.length));
+
+  return {
+    language,
+    ...locationData
+  };
+};
+
+export const parseWG = body => {
+  const locationData = parseLocationData(body.slice(1, body.length));
+
+  return {
+    ...locationData
+  };
+};
+
+// **** 2. SEND COMMANDS ON PLATFORM ***** //
+
+export const parseCENTER = body => {
+  return {
+    centerNumber: body[0]
+  };
+};
+
+export const parseSLAVE = body => {
+  return {
+    assistanceNumber: body[0]
+  };
+};
+
+export const parsePW = body => {
+  return {
+    password: body[0]
+  };
+};
+
+export const parseCALL = body => {
+  return {
+    phoneNumber: body[0]
+  };
+};
+
+export const parseSMS = body => {
+  return {
+    phoneNumber: body[0],
+    message: body[1]
+  };
+};
+
+export const parseUPGRADE = body => {
+  return {
+    URL: body[0]
+  };
+};
+
+export const parseIP = body => {
+  return {
+    IP: body[0],
+    port: parseInt(body[1])
+  };
+};
+
+export const parseLZ = body => {
+  return {
+    language: body[0],
+    timeArea: body[1]
   };
 };
